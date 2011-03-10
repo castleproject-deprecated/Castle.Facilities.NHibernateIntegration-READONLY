3183968ad2de5c8aab1f6eda72c0898638744380
jmuscott <jordan@.unity-is.com>
2011-03-10 10:06:15 +0000

    Added missing test files

---
+++ src/Castle.Facilities.NHibernateIntegration.Tests/Transactions/Model/ReadOnlyTransactionsService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Services.Transaction;
+using NHibernate;
+using NUnit.Framework;
+using Castle.Facilities.AutoTx;
+using ITransaction = Castle.Services.Transaction.ITransaction;
+
+namespace Castle.Facilities.NHibernateIntegration.Tests.Transactions.Model
+{
+    [Transactional]
+    public class ReadOnlyTransactionsService
+    {
+        private BlogDao dao;
+        private ISessionManager sessionManager;
+
+        public ReadOnlyTransactionsService(ISessionManager sessionManager,  BlogDao dao)
+		{
+			this.dao = dao;
+			this.sessionManager = sessionManager;
+		}
+
+		[Transaction(TransactionMode.Requires, ReadOnly = true )]
+		public virtual void ReadOnlyCall()
+		{
+			ISession session = sessionManager.OpenSession();
+			Assert.AreEqual(FlushMode.Never, session.FlushMode);
+		}
+
+		[Transaction(TransactionMode.Requires)]
+		public virtual void WriteCall()
+		{
+			ISession session = sessionManager.OpenSession();
+			Assert.AreEqual(sessionManager.DefaultFlushMode, session.FlushMode);
+		}
+
+
+    }
+}
---
+++ src/Castle.Facilities.NHibernateIntegration.Tests/Transactions/ReadOnlyTransactionsTestCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Castle.Facilities.AutoTx;
+using Castle.MicroKernel.Registration;
+using Castle.Facilities.NHibernateIntegration.Tests.Transactions.Model;
+using Castle.Services.Transaction;
+using NHibernate;
+
+
+namespace Castle.Facilities.NHibernateIntegration.Tests.Transactions
+{
+    [TestFixture]
+    public class ReadOnlyTransactionsTestCase : AbstractNHibernateTestCase
+    {
+        protected override void ConfigureContainer()
+        {
+            container.AddFacility("transactions", new TransactionFacility());
+
+            container.Register(Component.For<ReadOnlyTransactionsService>().Named("readOnlyTestService"));
+            container.Register(Component.For<BlogDao>().Named("blogdao"));
+        }
+            
+
+        [Test]
+        public void TestFlushMode()
+        {
+            ReadOnlyTransactionsService service = container.Resolve<ReadOnlyTransactionsService>();
+            ISessionManager sessionManager = container.Resolve<ISessionManager>();
+            ISession session = sessionManager.OpenSession();
+
+            service.ReadOnlyCall();
+            
+            service.WriteCall();
+            
+        }
+
+  
+    }
+}
